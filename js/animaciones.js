a4a778fb71c7ec73a929e2459ad5a76ad37875de
GuilleCaffa <guillermo.caffarini@gmail.com>
2023-05-28 12:42:29 -0300

    correcciones menores
    Correccion de errores ortográficos

--- js/animaciones.js
+++ js/animaciones.js
@@ -1,5 +1,5 @@
 var Typed = new Typed(".multiple-text", {
-    strings: ["Grupo15, especialistas en desarollo Web", "Desarrolladores Fullstack Python"],
+    strings: ["Grupo15, especialistas en desarrollo Web", "Desarrolladores Fullstack Python"],
     typeSpeed: 50,
     backSpeed: 50,
     backDelay: 1000,
